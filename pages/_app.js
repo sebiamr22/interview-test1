5fdcfe3017a7419785e44d28f87e8e1b71bf919d
sebiamr22 <amariieusebiu18@stud.ase.ro>
2022-07-31 17:19:26 +0300

    2nd commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,29 @@ import React from 'react';
 import Head from 'next/head';
 import * as config from '../site.config';
 import '../css/index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Root = (props) => {
   const { Component, pageProps } = props;
   const { title, stylesheets } = config;
-
+  const queryClient = new QueryClient();
   return (
     <>
-      <Head>
-        <title>{title}</title>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>{title}</title>
 
-        {stylesheets.map((href) => (
-          <link key={href} rel="stylesheet" href={href} />
+          {stylesheets.map((href) => (
+            <link key={href} rel="stylesheet" href={href} />
 
-        ))}
-        {/* <script src="https://cdn.tailwindcss.com"></script> */}
-      </Head>
-      <Component {...pageProps} />
+          ))}
+          {/* <script src="https://cdn.tailwindcss.com"></script> */}
+        </Head>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   );
 };
 
 export default Root;
+
